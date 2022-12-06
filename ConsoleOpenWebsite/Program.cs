5293b91ef6025de9886a7ea9697921968f277cc2
Apisit Sirichoke (อภิสิทธิ์ ศิริโชค) <Apisit.Sirichoke@quantaqmb.com>
2022-12-06 14:43:30 +0700

    First commit

---
+++ ConsoleOpenWebsite/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleOpenWebsite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //System.Diagnostics.Process.Start("10.97.1.26//SMT_DeleteSNDupricate//index.aspx");
+            System.Diagnostics.Process.Start("http://10.97.1.26//SMT_DeleteSNDupricate//index.aspx");
+        }
+    }
+}
